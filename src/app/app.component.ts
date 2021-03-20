fca4af2db32140ebc3316238014dff1ef75e5d3f
javierfm <jadosfer@hotmail.com>
2021-03-20 18:00:32 -0300

    049 Prevent XSS (Cross Site Scripting)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { LoginService } from './login.service';
 
 @Component({
@@ -8,9 +9,15 @@ import { LoginService } from './login.service';
 })
 export class AppComponent
 {
-  constructor(public loginService : LoginService)
+  constructor(public loginService : LoginService, private domSanitizer: DomSanitizer)
   {
   }
+
+  myProperty = "<svg>blah</svg>"; //prueba de seguridad XSS
+  //myProperty2 = "<script>alert(document.cookie)</script>"; 
+  myProperty2 = this.domSanitizer.bypassSecurityTrustHtml("<iframe src='http://www.lipsum.com'></iframe>");
+  myProperty3 = this.domSanitizer.bypassSecurityTrustUrl("javascript:window.open('http://www.google.com')");
+  myProperty4 = this.domSanitizer.bypassSecurityTrustResourceUrl("https://www.focus2move.com/wp-content/uploads/2020/01/Tesla-Roadster-2020-1024-03.jpg");
 
   onSearchClick()
   {
