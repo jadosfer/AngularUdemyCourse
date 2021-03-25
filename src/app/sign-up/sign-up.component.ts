f8b197cb609474cf9627bc5a7c23309d4b764ea7
javierfm <jadosfer@hotmail.com>
2021-03-24 22:59:09 -0300

    058 Dynamic DropDownList

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import {CountriesService} from '../countries.service';
+import { Country } from '../country';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,18 +12,22 @@ export class SignUpComponent implements OnInit
 {
   signUpForm: FormGroup;
   genders = ["male", "female"];
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(private countriesService: CountriesService) { }
 
   ngOnInit()
   {
+    this.countries = this.countriesService.getCountries();
+
     this.signUpForm = new FormGroup({
       firstName: new FormControl(null),
       lastName: new FormControl(null),
       email: new FormControl(null),
       mobile: new FormControl(null),
       dateOfBirth: new FormControl(null),
-      gender: new FormControl(null)
+      gender: new FormControl(null),
+      countryID: new FormControl(null)
     });
   }
 }
