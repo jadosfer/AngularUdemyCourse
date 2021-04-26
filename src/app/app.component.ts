b25b8f1865ba130f01ff63ea1e80e5e377687676
javierfm <jadosfer@hotmail.com>
2021-04-25 22:26:47 -0300

    110 Server Side Logging using Router Events nota creo un txt con los lugares donde navega el usuario dentro de mi app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
 import { DomSanitizer } from '@angular/platform-browser';
+import { RouterLoggerService } from './router-logger.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,24 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class AppComponent
 {
-  constructor(public loginService : LoginService, private domSanitizer : DomSanitizer)
+  constructor(public loginService : LoginService, private domSanitizer : DomSanitizer, 
+  private routerLoggerService: RouterLoggerService, private router: Router)
   {
   }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd)
+      {
+        let userName = (this.loginService.currentUserName)? this.loginService.currentUserName : "anonymous";
+        
+        let logMsg = new Date().toLocaleString() + ": " + userName + " navigates to " + event.url;
+
+        this.routerLoggerService.log(logMsg).subscribe();
+      }
+    });
+  }
+
   
   onSearchClick()
   {
