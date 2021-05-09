526ba89bb785d529b9169592a26f330394d2bfa7
javierfm <jadosfer@hotmail.com>
2021-05-09 19:32:27 -0300

    140 Update Task Status Page

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Task } from '../models/task';
 import { Observable } from 'rxjs';
 import { GroupedTask } from '../models/grouped-task';
+import { TaskStatusDetail } from '../models/task-status-detail';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,16 @@ export class TasksService
   getTasks() : Observable<GroupedTask[]>
   {
     return this.httpClient.get<GroupedTask[]>("/api/tasks", { responseType: "json" });
+  }
+
+  getTaskByTaskID(TaskID: number) : Observable<Task>
+  {
+    return this.httpClient.get<Task>("/api/tasks/searchbytaskid/" + TaskID, { responseType: "json" });
+  }
+
+  updateTaskStatus(taskStatusDetail: TaskStatusDetail) : Observable<TaskStatusDetail>
+  {
+    return this.httpClient.put<TaskStatusDetail>("/api/updatetaskstatus", taskStatusDetail, { responseType: "json" });
   }
 
   insertTask(newTask: Task): Observable<Task>
