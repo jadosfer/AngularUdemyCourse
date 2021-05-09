0dee04b825db58556cc838cb64127113a2f7bde7
javierfm <jadosfer@hotmail.com>
2021-05-09 18:08:44 -0300

    139 Apply Contextual Classes to Tasks

--- src/app/employee/components/tasks/tasks.component.ts
+++ src/app/employee/components/tasks/tasks.component.ts
@@ -22,5 +22,48 @@ export class TasksComponent implements OnInit
       this.taskGroups = response;
     });
   }
-}
 
+  /* Get background color based on task status */
+  getTaskGroupBgCssClass(taskStatusName) : string
+  {
+    var className;
+    switch (taskStatusName)
+    {
+      case "Holding": className = "bg-secondary text-white"; break; //gray
+      case "Prioritized": className = "bg-primary text-white"; break; //blue
+      case "Started": className = "bg-info text-white"; break; // light
+      case "Finished": className = "bg-success text-white"; break; //green
+      case "Reverted": className = "bg-danger text-white"; break; //red
+    }
+    return className;
+  }
+
+  /* Get background color based on task priority */
+  getTaskPriorityBadgeCssClass(taskPriorityName) : string
+  {
+    var className;
+    switch (taskPriorityName)
+    {
+      case "Urgent": className = "badge-danger"; break; //red
+      case "Normal": className = "badge-primary"; break; //blue
+      case "Below Normal": className = "badge-info"; break; // light blue
+      case "Low": className = "badge-secondary"; break; // gray
+    }
+    return className;
+  }
+
+  /* Get text color based on task status */
+  getTaskGroupTextCssClass(taskStatusName) : string
+  {
+    var className;
+    switch (taskStatusName)
+    {
+      case "Holding": className = "text-secondary"; break; //gray
+      case "Prioritized": className = "text-primary"; break; //blue
+      case "Started": className = "text-info"; break;// light blue
+      case "Finished": className = "text-success"; break; //green
+      case "Reverted": className = "text-danger"; break; //red
+    }
+    return className;
+  }
+}
